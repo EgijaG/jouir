17136bb40f5273555fee2933086f7d132b803702
EgijaG <egija.graudina@gmail.com>
2023-09-30 23:57:26 +0300

    fixed the request conditions, ready for title, text parsing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # read config file, so every changing value could be changed outside code
+import sys
 import time
 
 import yaml
@@ -22,23 +23,24 @@ config_data = read_config()
 # Find web request library
 def get_post(provided_tags):
     print('Hello from getting single post or multiple')
-    count = 0
-    while count < 3:
-        newest_post = get_newest_post_with_vote(config_data['start-url'], config_data['path'],
-                                                config_data['posts-class-name'],
-                                                config_data['votes-class-name'], config_data['tags-class-name'],
-                                                provided_tags)
-        # later this has to be saved to DB
-        if newest_post:
-            print('Got the newest post with at least one vote:', newest_post)
-        count += 1
-        time.sleep(60)
+
+    newest_post = get_newest_post_with_vote(config_data['start-url'], config_data['path'],
+                                            config_data['posts-class-name'],
+                                            config_data['votes-class-name'],
+                                            provided_tags, config_data['post_text_excerpt_class'],
+                                            config_data['post_title_class'])
+
+    # later this has to be saved to DB
+    if newest_post:
+        print('Got the newest post with at least one vote:', newest_post)
+        return newest_post
 
 
 # ask the user for tags they want to see / read tags from config file
 tags = get_tags_from_user_input(config_data['single-tag-class-name'])
 print("Got the tags from user -> ", tags)
-get_post(tags)
+print("Final post ->", get_post(tags))
+sys.exit(0)
 # create a database connection (outside this file do all DB things - table, db, rights, connection etc.)
 
 # save the newest post, that has at least 1 vote
--- web_requests.py
+++ web_requests.py
@@ -1,27 +1,47 @@
+import sys
+import time
+
 import requests
 from bs4 import BeautifulSoup
 
 
 # make a request with 1 min timeout
-def get_newest_post_with_vote(url, path, posts_class, votes_class, tags_class, provided_tags):
+def get_newest_post_with_vote(url, path, posts_class, votes_class, provided_tags, post_excerpt_class, post_title_class):
     print("found my url", url + path)
-    response = requests.get(url=url + path, timeout=1)
-    print(response)
-    result_set = []
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        posts = soup.select(posts_class)
-# need to fix it to only get ones with at least one vote
-        for post in posts:
-            print(post.text, " post and tag ", provided_tags)
-            # Replace "vote_selector" with the CSS selector for the vote count element
-            vote_count = post.select_one(votes_class)
+    switch_to_next_page: bool = True
+    page_number = 1
+    result_set = {}
+    while switch_to_next_page:
+        response = requests.get(url=url + path + str(page_number), timeout=1)
+        print("Full url: ", url + path + str(page_number))
+        print(response.status_code)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            posts = soup.select(posts_class)
+            # need to fix it to only get ones with at least one vote
+            for post in posts:
+                print(" post and tag ", provided_tags)
+                vote_count = post.select_one(votes_class)
+                print(vote_count.text, ' -> vote count')
+                if int(vote_count.text) < 1:
+                    print("Vote count less than one, skipping this post", vote_count.text)
+                else:
+                    # Check if any of the specified tags are found in the post text
+                    print('muhaha got in the else statement')
+                    for tag in provided_tags:
+                        print(tag, "found the single tag")
+                        post_with_tag = post.select_one(tag)
+                        if post_with_tag:
+                            print("found the post that has corresponding tag and at least one vote")
+                            # TODO need to figure out how to get title and text from post
+                            result_set[post.select_one(post_title_class).text] = (post.select_one(post_excerpt_class)
+                                                                                  .text)
+                            switch_to_next_page = False
+        if len(result_set) < 1:
+            print("No posts with those tags that meet the criteria found on this page. Let's check the next "
+                  "page...")
+            page_number += 1
+            time.sleep(60)
+            print("page number - ", page_number)
 
-            if vote_count and int(vote_count.text) >= 1:
-                # Check if any of the specified tags are found in the post text
-                for tag in provided_tags:
-                    print(tag, "found the single tag")
-                    res = post.select_one(tag)
-                    if res:
-                        result_set.append(res)
     return result_set
